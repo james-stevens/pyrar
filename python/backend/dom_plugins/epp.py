339e47da78ddbc1b795af2dd04d791be3e9221d7
James Stevens <github@jrcs.net>
2025-09-18 11:43:36 +0000

    copyright

--- python/backend/dom_plugins/epp.py
+++ python/backend/dom_plugins/epp.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-# (c) Copyright 2019-2022, James Stevens ... see LICENSE for details
+# (c) Copyright 2019-2025, James Stevens ... see LICENSE for details
 # Alternative license arrangements possible, contact me for more information
 """ bacnend call backs to run requests to an EPP registry """
 
